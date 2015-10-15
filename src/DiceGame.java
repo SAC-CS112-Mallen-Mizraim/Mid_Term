054d3016e01f478ebe9959757651378172325e5c
SAC-CS112-Mallen-Mizraim <mizmallen@gmail.com>
2015-10-14 21:11:09 -0700

    Mid Term test.

---
+++ src/DiceGame.java
@@ -0,0 +1,37 @@
+/*
+ * Miz Mallen Dice Game Mid Term
+ * 
+ * I've got a lot of studying to do. 
+ */
+
+import java.util.Random;
+import java.util.Scanner;
+
+
+public class DiceGame{
+	public static void main(String[] args){
+		
+		//Declaration Statement
+		String choice1;
+		
+		System.out.println("Would you like to play a dice game\n?" +" Type y for yes, or n for no." );
+		choice1 = scan.next();
+		
+		if (choice1 == y){
+		System.out.println("Okay then, here we go.")}
+		
+		else if 
+		
+		
+		Random dice = new Random();
+		int number;
+		
+		for(int counter=1; counter<=6; counter++){
+			number = 1 + dice.nextInt(6);
+			System.out.println(number + " ");
+			
+		}
+	
+		
+	}
+}
